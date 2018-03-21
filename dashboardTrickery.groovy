3f7017fc7370a24f6f5758dd2772292ecbf673a1
Ian Bloom <ian.bloom@gmail.com>
2018-03-21 15:48:17 -0700

    Add files via upload
    Updated to use helper classes in functionstest.groovy

--- dashboardTrickery.groovy
+++ dashboardTrickery.groovy
@@ -1,6 +1,8 @@
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.util.EntityUtils;
@@ -8,42 +10,26 @@ import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import org.apache.commons.codec.binary.Hex;
 import groovy.json.JsonSlurper;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.ContentType;
 
 //define credentials and url
 def accessId = 'dSpe6j9eTQXs3Iph7jCU'
 def accessKey = 'dcm!p2d2w79V=5f}+[354xL=g{k442Y6h5qV}C_6'
 def account = 'ianbloom'
+def rootGroup = '39'
 
 def resourcePath = '/device/groups'
-def queryParameters = '?fields=name,id,customProperties&filter=parentId~39'
+def queryParameters = '?fields=name,id,customProperties&filter=parentId~' + rootGroup;
 def url = 'https://' + account + '.logicmonitor.com' + '/santaba/rest' + resourcePath + queryParameters;
 def data = '' // Single quoted as contents include double quotes. Only used when updating data, but being blank elsewise shouldn't hurt. In for template purposes.
+def requestVerb = 'GET'
 
-//get current time
-epoch = System.currentTimeMillis();
+groupDict = LMGET(accessId, accessKey, account, requestVerb, resourcePath, queryParameters, data)
+holderDictArray = [];
 
-//calculate signature
-requestVars = 'GET' + epoch + data + resourcePath;
-
-hmac = Mac.getInstance('HmacSHA256');
-secret = new SecretKeySpec(accessKey.getBytes(), 'HmacSHA256');
-hmac.init(secret);
-hmac_signed = Hex.encodeHexString(hmac.doFinal(requestVars.getBytes()));
-signature = hmac_signed.bytes.encodeBase64();
-
-// HTTP Get
-CloseableHttpClient httpclient = HttpClients.createDefault();
-httpGet = new HttpGet(url);
-httpGet.addHeader('Authorization' , 'LMv1 ' + accessId + ':' + signature + ':' + epoch);
-response = httpclient.execute(httpGet);
-responseBody = EntityUtils.toString(response.getEntity());
-code = response.getStatusLine().getStatusCode();
-
-holderDictArray = []
-
-// If HTTP GET for subgroups of root is successful, obtain name, ID, and location custom property
-if (code == 200) {
-	
+if (groupDict.code == 200) {
+	responseBody = groupDict.body;
 	rootGroups = new JsonSlurper().parseText(responseBody);
 	groupArray = rootGroups.data.items;
 	groupArrayLength = groupArray.size();
@@ -70,7 +56,6 @@ if (code == 200) {
 		}
 	}
 }
-
 holderDictArray.each { item ->
 	print(item['groupName']);
 	print("\n")
@@ -81,35 +66,80 @@ holderDictArray.each { item ->
 	print("\n")
 }
 
-// LETS MAKE A DASHBOARD FOR THE ROOT FOLDER, THEN DASHBOARDS FOR THE STATES, THEN LINK THEM UP
 
-holderDictArray.each { item, index ->
 
-	resourcePath = '/dashboard/dashboards'
-	queryParameters = '?name=' + item['groupName']
-	url = 'https://' + account + '.logicmonitor.com' + '/santaba/rest' + resourcePath + queryParameters;
-	data = '' // Single quoted as contents include double quotes. Only used when updating data, but being blank elsewise shouldn't hurt. In for template purposes.
 
-	//get current time
+def LMGET(_accessId, _accessKey, _account, _requestVerb, _resourcePath, _queryParameters, _data) {
+	// DATA SHOULD BE EMPTY
+	// Initialize dictionary to hold response code and response body
+	responseDict = [:];
+
+	// Construcst URL to POST to from specified input
+	url = 'https://' + _account + '.logicmonitor.com' + '/santaba/rest' + _resourcePath + _queryParameters;
+
+	// Get current time
 	epoch = System.currentTimeMillis();
 
-	//calculate signature
-	requestVars = 'POST' + epoch + data + resourcePath;
+	// Calculate signature
+	requestVars = _requestVerb + epoch + _data + _resourcePath;
 
 	hmac = Mac.getInstance('HmacSHA256');
-	secret = new SecretKeySpec(accessKey.getBytes(), 'HmacSHA256');
+	secret = new SecretKeySpec(_accessKey.getBytes(), 'HmacSHA256');
 	hmac.init(secret);
 	hmac_signed = Hex.encodeHexString(hmac.doFinal(requestVars.getBytes()));
 	signature = hmac_signed.bytes.encodeBase64();
 
 	// HTTP Get
 	CloseableHttpClient httpclient = HttpClients.createDefault();
-	httpGet = new HttpGet(url);
-	httpGet.addHeader('Authorization' , 'LMv1 ' + accessId + ':' + signature + ':' + epoch);
-	response = httpclient.execute(httpGet);
+	http_request = new HttpGet(url);
+	http_request.addHeader("Authorization" , "LMv1 " + _accessId + ":" + signature + ":" + epoch);
+	response = httpclient.execute(http_request);
 	responseBody = EntityUtils.toString(response.getEntity());
 	code = response.getStatusLine().getStatusCode();
+
+	responseDict['code'] = code;
+	responseDict['body'] = responseBody
+	
+	return responseDict;
 }
 
+def LMPOST(_accessId, _accessKey, _account, _requestVerb, _resourcePath, _queryParameters, _data) {
+
+	// Initialize dictionary to hold response code and response body
+	responseDict = [:];
+
+	// Construcst URL to POST to from specified input
+	url = 'https://' + _account + '.logicmonitor.com' + '/santaba/rest' + _resourcePath + _queryParameters;
+
+	StringEntity params = new StringEntity(_data,ContentType.APPLICATION_JSON);
+
+	// Get current time
+	epoch = System.currentTimeMillis();
+
+	// Calculate signature
+	requestVars = _requestVerb + epoch + _data + _resourcePath;
+
+	hmac = Mac.getInstance('HmacSHA256');
+	secret = new SecretKeySpec(_accessKey.getBytes(), 'HmacSHA256');
+	hmac.init(secret);
+	hmac_signed = Hex.encodeHexString(hmac.doFinal(requestVars.getBytes()));
+	signature = hmac_signed.bytes.encodeBase64();
+
+	// HTTP Get
+	CloseableHttpClient httpclient = HttpClients.createDefault();
+	http_request = new HttpPost(url);
+	http_request.addHeader("Authorization" , "LMv1 " + _accessId + ":" + signature + ":" + epoch);
+	http_request.setHeader("Accept", "application/json");
+	http_request.setHeader("Content-type", "application/json");
+	http_request.setEntity(params);
+	response = httpclient.execute(http_request);
+	responseBody = EntityUtils.toString(response.getEntity());
+	code = response.getStatusLine().getStatusCode();
+
+	responseDict['code'] = code;
+	responseDict['body'] = responseBody
+	
+	return responseDict;
+}
 
 return 0;
